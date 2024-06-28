9bdeee85f23ebc33a71edcdb6f9ab9f7e6ca7ea0
Alanmenga <alan.gabriel.mengassini@gmail.com>
2024-06-28 09:35:07 -0300

    inicializando proyecto

---
+++ src/main/java/com/example/alan/model/Category.java
@@ -0,0 +1,42 @@
+package com.example.alan.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="category")
+public class Category implements Serializable {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
